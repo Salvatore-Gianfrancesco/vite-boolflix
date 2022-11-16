5af517e9dab3e4cc0284bc596736d910c77f8096
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-11-16 12:10:35 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<template>
+  <div class="test text-center">
+    <h1>Test</h1>
+  </div>
+</template>
+
+<style lang='scss'>
+.test {
+  background-color: gray;
+
+  h1 {
+    color: white;
+  }
+}
+</style>
